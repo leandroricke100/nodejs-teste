bb71fbff5e4e7334fe4c48484e79d6c96c4bcd6b
leandroricke100 <leandro_henrique_100@hotmail.com>
2023-08-18 16:07:53 -0300

    update

--- modules/express.js
+++ modules/express.js
@@ -2,38 +2,76 @@ const express = require("express");
 const UserModel = require("../src/models/user.model");
 
 const app = express();
+const port = 8080;
 
 app.use(express.json());
+app.set("view engine", "ejs");
+app.set("views", "src/views");
 
-app.get("/home", (req, res) => {
-  res.status(200).send("<h1>Hello word</h1>");
+app.use((req, res, next) => {
+  console.log(`Request Type: ${req.method}`);
+  console.log(`Content Type: ${req.headers["content-type"]}`);
+  console.log(`Date: ${new Date()}`);
+  next();
 });
 
-app.get("/users", (req, res) => {
-  const users = [
-    {
-      name: "Leandro",
-      email: "leandro@email.com",
-    },
-    {
-      name: "Leticia",
-      email: "leticia@email.com",
-    },
-  ];
+app.get("/views/users", async (req, res) => {
+  try {
+    const users = await UserModel.find({});
+    res.render("index", { users });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 
-  res.status(200).json(users);
+app.get("/users", async (req, res) => {
+  try {
+    const users = await UserModel.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.get("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findById(id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
 app.post("/users", async (req, res) => {
   try {
     const user = await UserModel.create(req.body);
-
     res.status(201).json(user);
   } catch (error) {
     res.status(500).send(error.message);
   }
 });
 
-const port = 8080;
+app.patch("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 
-app.listen(port, () => console.log(`Rodando com express na porta ${port}`));
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findByIdAndDelete(id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.listen(port, () =>
+  console.log(`Express server is running on port ${port}`)
+);
